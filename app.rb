58e13d6f6d412c29b7787c284f51b812bd3c2521
kaakaa <stooner.hoe@gmail.com>
2015-03-01 01:09:58 +0900

    top page

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require 'json'
+require 'haml'
 
 get '/' do
-	p 'hello'
+	@projects = Dir.glob('./build/*')
+	haml :index
+end
+
+get '/:project' do
+	project_name = params[:project]
+	log_file = "./build/#{project_name}/build.log"
+	if File.exists?(log_file) then
+		@text= File.open(log_file).read
+	else
+		@text= "NO BUILD LOG"
+	end
+	haml :log
 end
 
 post '/hook' do
@@ -19,7 +32,7 @@ post '/hook' do
     p `git clone #{url} #{project_root}`
 	  `chmod +x #{project_root}/gradlew`
 	end
-	p `#{project_root}/gradlew build -p #{project_root}`
+	`#{project_root}/gradlew build -p #{project_root} > #{project_root}/build.log`
 
 	if $? then
 		p 'BUILD SUCCECC'
@@ -28,3 +41,22 @@ post '/hook' do
 	end
 end
 
+__END__
+
+@@ layout
+%html
+  %head
+    %title= 'builder'
+  %body
+    = yield
+    %a{:href => "/"}= "TOP"
+
+@@ index
+%h1= 'Projects'
+%ul
+  - @projects.each do |p|
+    %li
+      %a{:href => "/#{File.basename(p)}"}= "#{File.basename(p)}"
+
+@@ log
+%pre= @text
